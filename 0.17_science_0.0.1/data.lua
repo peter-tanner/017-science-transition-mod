262297b903a37686bbf389f9a1ece5bc21965720
npc-strider <npc.strider@gmail.com>
2018-12-31 20:37:50 +0800

    cleanup

--- 0.17_science_0.0.1/data.lua
+++
@@ -1,7 +0,0 @@
-require("recipe")
-
-table.insert(data.raw["technology"]["military-2"].effects, {type = "unlock-recipe",recipe = "17-military-science-pack"})
-table.insert(data.raw["technology"]["advanced-electronics"].effects, {type = "unlock-recipe",recipe = "17-chemical-science-pack"})
-table.insert(data.raw["technology"]["advanced-material-processing-2"].effects, {type = "unlock-recipe",recipe = "17-production-science-pack"})
-table.insert(data.raw["technology"]["advanced-electronics-2"].effects, {type = "unlock-recipe",recipe = "17-utility-science-pack"})
-table.insert(data.raw["technology"]["advanced-electronics-2"].effects, {type = "unlock-recipe",recipe = "low-density-structure"}) --LDS unlock for pre-rocket bases
