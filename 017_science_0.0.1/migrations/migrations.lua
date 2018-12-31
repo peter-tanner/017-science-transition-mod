6d796a1b01f335e81a40ea1c59a4957417e13b42
npc-strider <npc.strider@gmail.com>
2018-12-31 20:32:28 +0800

    changed names for mod portal

---
+++ 017_science_0.0.1/migrations/migrations.lua
@@ -0,0 +1,23 @@
+for i, force in pairs(game.forces) do 
+	force.reset_recipes()
+end
+
+for i, force in pairs(game.forces) do 
+	force.reset_technologies()
+end
+
+for i, force in pairs(game.forces) do 
+	if force.technologies["military-2"].researched then 
+		force.recipes["17-military-science-pack"].enabled = true
+	end
+	if force.technologies["advanced-electronics"].researched then 
+		force.recipes["17-chemical-science-pack"].enabled = true
+	end
+	if force.technologies["advanced-material-processing-2"].researched then 
+		force.recipes["17-production-science-pack"].enabled = true
+	end
+	if force.technologies["advanced-electronics-2"].researched then 
+		force.recipes["low-density-structure"].enabled = true
+		force.recipes["17-utility-science-pack"].enabled = true
+	end
+end
