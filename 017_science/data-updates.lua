aaabba2d544e0266de240f3aef1be4ed2d8a519c
npc-strider <npc.strider@gmail.com>
2019-01-03 17:54:50 +0800

    productivity modules

---
+++ 017_science/data-updates.lua
@@ -0,0 +1,17 @@
+--whitelist for productivity module effect
+for _, mod in pairs(data.raw.module) do
+	if mod.effect ~= nil then
+		for _, effect in pairs(mod.effect) do
+			if effect[1] == productivity then
+				if mod.limitation ~= nil then
+					local limitation = mod.limitation
+					--new stuff
+					table.insert(limitation, "17-military-science-pack")
+					table.insert(limitation, "17-chemical-science-pack")
+					table.insert(limitation, "17-production-science-pack")
+					table.insert(limitation, "17-utility-science-pack")
+				end
+			end
+		end
+	end
+end
