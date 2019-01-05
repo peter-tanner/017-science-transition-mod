8cf9f6dd6f31aab9fac205b9413a1eb3517c603e
npc-strider <npc.strider@gmail.com>
2019-01-05 13:36:07 +0800

    options for latest 0.17 balancing

--- 017_science/data-updates.lua
+++ 017_science/data-updates.lua
@@ -17,6 +17,9 @@ for _, mod in pairs(data.raw.module) do
 	end
 end
 
+--HUGE THANKS to Dimava for the following changes
+--Added options for some changes (for modded users)
+
 --increase belt speed to 15x
 for _, belt in pairs(data.raw["splitter"]) do
 	if (belt.speed * 32) % 1 == 0 and belt.speed * 32 < 8 then
@@ -34,39 +37,45 @@ for _, belt in pairs(data.raw["underground-belt"]) do
 	end
 end
 
---simplify drill stats
-data.raw["mining-drill"]["burner-mining-drill"].mining_speed = 0.25
-data.raw["mining-drill"]["burner-mining-drill"].mining_power = 3
+if settings.startup["017-drill"].value then
+	--simplify drill stats
+	data.raw["mining-drill"]["burner-mining-drill"].mining_speed = 0.25
+	data.raw["mining-drill"]["burner-mining-drill"].mining_power = 3
 
---simplify ore hardness
-for _, ore in pairs(data.raw.resource) do
-	if ore.minable.hardness == 0.9 then
-		ore.minable.hardness = 1
+	--simplify ore hardness
+	for _, ore in pairs(data.raw.resource) do
+		if ore.minable.hardness == 0.9 then
+			ore.minable.hardness = 1
+		end
 	end
+	data.raw.resource.stone.minable.hardness = 1
 end
-data.raw.resource.stone.minable.hardness = 1
 
---decrease smelting times
-for _, recipe in pairs(data.raw.recipe) do
-	if recipe.category=="smelting" then
-		if recipe.energy_required then
-			if (recipe.energy_required / 3.5) % 1 == 0 then
-				recipe.energy_required = recipe.energy_required * 3.2 / 3.5
+if settings.startup["017-smelting"].value then
+	--decrease smelting times
+	for _, recipe in pairs(data.raw.recipe) do
+		if recipe.category=="smelting" then
+			if recipe.energy_required then
+				if (recipe.energy_required / 3.5) % 1 == 0 then
+					recipe.energy_required = recipe.energy_required * 3.2 / 3.5
+				end
+			else
+				if (recipe.normal.energy_required / 3.5) % 1 == 0 then
+					recipe.normal.energy_required = recipe.normal.energy_required * 3.2 / 3.5
+				end
+				if (recipe.expensive.energy_required / 3.5) % 1 == 0 then
+					recipe.expensive.energy_required = recipe.expensive.energy_required * 3.2 / 3.5
+				end			
 			end
-		else
-			if (recipe.normal.energy_required / 3.5) % 1 == 0 then
-				recipe.normal.energy_required = recipe.normal.energy_required * 3.2 / 3.5
-			end
-			if (recipe.expensive.energy_required / 3.5) % 1 == 0 then
-				recipe.expensive.energy_required = recipe.expensive.energy_required * 3.2 / 3.5
-			end			
 		end
 	end
 end
 
---remove assembling-machine ingredient_count limits
-for _, machine in pairs(data.raw["assembling-machine"]) do
-	if machine.ingredient_count < 10 then
-		machine.ingredient_count = 10
+if settings.startup["017-assem-lim"].value then
+	--remove assembling-machine ingredient_count limits
+	for _, machine in pairs(data.raw["assembling-machine"]) do
+		if machine.ingredient_count < 10 then
+			machine.ingredient_count = 10
+		end
 	end
 end
---
+++ 017_science/settings.lua
@@ -0,0 +1,21 @@
+data:extend(
+{
+    {
+        type = "bool-setting",
+        name = "017-drill",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "017-smelting",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "017-assem-lim",
+        setting_type = "startup",
+        default_value = true
+    }	
+})
